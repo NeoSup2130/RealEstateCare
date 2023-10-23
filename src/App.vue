c5879e6a6a57d1c4319e5d3584ef7c2c87f82e73
NeoSup2130 <slavaxbox@hotmail.nl>
2023-10-23 12:52:50 +0200

    [+] Basis VueJS project toegevoegd

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Real Estate Care logo" src="./assets/logo.png">
+  <Homepage msg="Welcome to Real Estate Care App"/>
+</template>
+
+<script>
+import Homepage from './components/Homepage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Homepage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(41,52,57);
+  background-color: white;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Wie zijn wij?</h2>
+    <p>
+      RealEstateCare is een dienstverlenende organisatie die voor verschillende opdrachtgever zoals
+      woningcoöperaties en vastgoedontwikkelaars woningen onderhoudt, verbetert en inspecteert. Hier
+      spelen veel en ingewikkelde bedrijfsprocessen en wordt veel data verwerkt. Eén aspect van deze
+      bedrijfsprocessen is het op locatie inspecteren van een pand en het opnemen van schade (door huurders,
+      ongedierte of calamiteiten), achterstallig onderhoud, verouderde installaties, brandgevaarlijke situaties en
+      al dan niet ongewenste modificaties van huurders.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  width: 45vw;
+  margin: 0 auto;
+}
+
+p {
+  text-align: justify;
+}
+a {
+  color: rgb(0,170,162);
+}
+</style>
