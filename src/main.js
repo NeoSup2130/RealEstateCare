10fa4f4051b2023bbd5b8f45e2faad38732ae820
NeoSup2130 <slavaxbox@hotmail.nl>
2023-10-30 00:41:53 +0100

    [+] Added Pinia for state management.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+// Pinia (State management)
+import {createPinia} from "pinia";
+
+const pinia = createPinia();
 
 // Router
 import router from "@/router";
 import routerFuncs from "@/mixins/routerFuncs";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
-createApp(App).use(vuetify).use(router).mixin(routerFuncs).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .use(router)
+  .use(pinia)
+  .mixin(routerFuncs)
+  .mount('#app');
