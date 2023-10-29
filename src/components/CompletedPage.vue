87878a8fb1c856cafc256036e02a48fd23a3d078
NeoSup2130 <slavaxbox@hotmail.nl>
2023-10-29 21:05:15 +0100

    [+] Added JSON-Server as fake back-end interface. [+] Created mock data for inspections. [+] Created view to display completed inspections.

--- src/components/CompletedPage.vue
+++ src/components/CompletedPage.vue
@@ -1,12 +1,57 @@
 <template>
-    <h2>Completed inspections</h2>
+    <h2 class="mt-2">Completed inspections</h2>
+    <v-divider class="w-50"></v-divider>
+    <div class="mx-4">
+    <v-expansion-panels>
+      <v-expansion-panel v-for="inspection in inspections" :key="inspection.id" class="ma-2">
+        <v-expansion-panel-title expand-icon="mdi-menu-down">
+          <v-row>
+            <v-col> Inspection Nr. {{ inspection.id }} </v-col>
+            <v-col> Date. {{ inspection.date }} </v-col>
+          </v-row>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text class="category-view">
+            <InspectionPanel v-for="(item, index) in inspection.items" :key="index" :inspection="item">
+            </InspectionPanel>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    </div>
 </template>
 
 <script>
+import InspectionPanel from "@/components/shared/InspectionPanel.vue";
+
     export default {
-        name: "CompletedView"
+        name: "CompletedView",
+        components: { InspectionPanel },
+        data() {
+            return {
+                inspections : null
+            }
+        },
+        methods : {
+            showInspection(e)
+            {
+                window.alert(e.id);
+            }
+        },
+        async created() 
+        {
+            const response = await fetch("http://localhost:3000/inspections?_sort=date&_order=desc", {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            });
+            const data = await response.json();
+            this.inspections = data;
+        }
     }
 </script>
 
 <style scoped>
+.category-view :deep(.v-expansion-panel-text__wrapper){
+    padding: 0 !important;
+}
 </style>
---
+++ src/components/shared/InspectionPanel.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-expansion-panels variant="accordion">
+        <v-expansion-panel v-for="(category, index) in Object.keys(inspection)" :key="index">
+            <v-expansion-panel-title expand-icon="mdi-menu-down">
+                {{ category }}
+            </v-expansion-panel-title>
+            <v-expansion-panel-text class="pa-4">
+                <table>
+                    <tbody>
+                        <tr v-for="[key, value, index] of Object.entries(inspection[category])" :key="index">
+                            <th>{{key}}:</th>
+                            <td>{{value}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </v-expansion-panel-text>
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
+
+<script>
+    export default {
+        name: "InspectionPanel",
+        props : {
+            inspection : Object
+        }
+    }
+</script>
+
+<style scoped>
+table {
+    width: 100%;
+}
+th, td {
+    width: 50%;
+    text-align: left;
+}
+th {
+    vertical-align: text-top;
+}
+</style>
