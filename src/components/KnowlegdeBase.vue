fe4b589d029b1f5fd2d7b77505f977df0967f325
NeoSup2130 <slavaxbox@hotmail.nl>
2023-10-29 23:54:54 +0100

    [+] Added default pdf document to link in KnowlegdeBase.

--- src/components/KnowlegdeBase.vue
+++ src/components/KnowlegdeBase.vue
@@ -1,10 +1,32 @@
 <template>
-    <h2>Here's the database!</h2>
+    <h2 class="mt-2">Knowlegde base</h2>
+    <v-divider class="w-50 mx-auto my-1" thickness="3"></v-divider>
+    <v-list lines="two" class="text-left">
+        <v-list-item
+        v-for="(pdf, index) in pdfs"
+        :key="index"
+        :title="pdf.name"
+        >
+        <a :href="'/pdf/' + pdf.file" target="_blank">{{pdf.file}}</a>
+        <template v-slot:prepend>
+          <v-icon icon="mdi-file-pdf-box"></v-icon>
+        </template>
+    </v-list-item>
+    </v-list>
+    
 </template>
 
 <script> 
 export default {
-    name: "KnowlegdeBase"
+    name: "KnowlegdeBase",
+    data() {
+        return {
+            pdfs : [
+                {file : "originaldocument.pdf", name : "Real Estate Care case document"},
+                {file : "originaldocument.pdf", name : "Test procedures"}
+            ]
+        }
+    }
 }
 </script>
 
