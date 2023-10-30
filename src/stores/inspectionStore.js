67e333fec4fe0fa44764d9754e8c89d3511148df
NeoSup2130 <slavaxbox@hotmail.nl>
2023-10-30 02:15:06 +0100

    [+] Implemented InspectionStore.

---
+++ src/stores/inspectionStore.js
@@ -0,0 +1,45 @@
+import {defineStore} from "pinia";
+import axios from 'axios';
+
+let port = "3000";
+const url = "http://localhost:"+port+"/inspections"; 
+
+export const useInspectionStore = defineStore('inspection', 
+{
+    state: () => {
+        return {
+            loadingState : 'notLoading',
+            inspections : [],
+            errors : null
+        }
+    },
+    actions : {
+        fetchInspections()
+        {
+            this.loadingState = 'loading';
+            axios.get(url + '?_sort=date&_order=desc')
+            .then(result => {
+                this.loadingState = 'notLoading';
+                this.inspections = result.data;
+            })
+            .catch(error => {
+                this.loadingState = 'notLoading';
+                this.inspections = [];
+                this.errors = error;
+
+            })
+        },
+        clearInspections()
+        {
+            this.loadingState = 'notLoading';
+            this.inspections = [];
+            this.errors = null;
+        }
+    },
+    getters : {
+        isLoading()
+        {
+            return this.loadingState === 'loading';
+        }
+    }
+})
